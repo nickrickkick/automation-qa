1f40029e81e6fe9e6a8b7f6a369c883c3a950360
Michael Nowlin <michaelnowlin5@gmail.com>
2022-05-03 20:25:19 -0600

    finished 3 test for movieList

--- movieList/functions.js
+++ movieList/functions.js
@@ -1,1 +1,98 @@
 const { By } = require('selenium-webdriver');
+
+const addMovie = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+  
+    addMovieInput.sendKeys('Back to the future');
+  
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+  
+    addMovieButton.click();
+  
+    await driver.sleep(1000);
+  
+    const movie = await driver.findElement(By.xpath('//li'));
+  
+    const isDisplayed = movie.isDisplayed();
+  
+    expect(isDisplayed).toBeTruthy();
+};
+
+const deleteMovie = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+  
+    addMovieInput.sendKeys('Hellboy');
+  
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+  
+    addMovieButton.click();
+  
+    await driver.sleep(1000);
+  
+    const movieXButton = await driver.findElement(By.xpath(`//li/button`))
+
+    movieXButton.click();
+
+    await driver.sleep(1000);   
+
+    const movieDeleteText = await driver.findElement(By.xpath(`//aside`))
+  
+    const isDisplayed = movieDeleteText.isDisplayed();
+
+  
+    expect(isDisplayed).toBeTruthy();
+};
+const crossOff = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+  
+    addMovieInput.sendKeys('cars');
+  
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+  
+    addMovieButton.click();
+  
+    await driver.sleep(1000);
+  
+    const movieCrossOff = await driver.findElement(By.xpath(`//li/span`))
+
+    movieCrossOff.click();
+
+    await driver.sleep(2000);   
+
+    const movieCrossOffText = await driver.findElement(By.xpath(`//aside`))
+  
+    const isDisplayed = movieCrossOffText.isDisplayed();
+
+  
+    expect(isDisplayed).toBeTruthy();
+};
+const crossGone = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+  
+    addMovieInput.sendKeys('sonic 2');
+  
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+
+    addMovieButton.click();
+  
+    await driver.sleep(1000);
+  
+    const movieCrossGone = await driver.findElement(By.xpath(`//li/span`))
+
+    movieCrossGone.click();
+
+    await driver.sleep(1000);
+
+    movieCrossGone.click();
+
+    await driver.sleep(2000);   
+
+    const movieCrossGoneText = await driver.findElement(By.xpath(`//aside`))
+  
+    const isDisplayed = movieCrossGoneText.isDisplayed();
+
+  
+    expect(isDisplayed).toBeTruthy();
+};
+
+module.exports = { addMovie, deleteMovie, crossOff, crossGone };
--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -4,6 +4,8 @@ require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
+const { addMovie, deleteMovie, crossOff, crossGone } = require('./functions');
+
 beforeEach(async () => {
     await driver.get('http://localhost:5500/movieList/index.html');
   });
@@ -12,5 +14,19 @@ beforeEach(async () => {
     await driver.sleep(1000);
     await driver.quit();
   });
+  //this from the lab. using it for ref.
+  it('should add new movie to list', async () => {
+    await addMovie(driver);
+  });
 
-  
+  it("should delete the new movie", async () => {
+    await deleteMovie(driver);
+  });
+
+  it("should cross off the new movie", async () => {
+    await crossOff(driver);
+  });
+
+  it("should cross gone the new movie", async () => {
+    await crossGone(driver);
+  });
